5d7011618f1adf93b0eaabe4c62961892ee6747a
Andreas Røsdal <andreas.rosdal@gmail.com>
2026-07-17 19:45:36 +0200

    history: declare ns_history_record only in the embed header

--- src/history.h
+++ src/history.h
@@ -8,12 +8,13 @@
 
 #include <glib.h>
 
+#include "libnordstjernen.h"
+
 G_BEGIN_DECLS
 
 void   ns_history_init(void);
 void   ns_history_shutdown(void);
 
-void   ns_history_record(const char *url, const char *title);
 void   ns_history_clear(void);
 
 char  *ns_history_html_page(void);
